71a8c4a5280c7d9b4de16a46d222988a85c43392
David McCullah <davidwmccullah@gmail.com>
2022-08-18 03:47:29 -0500

    Attempting to adjust wibar widgets.

--- rc.lua
+++ rc.lua
@@ -181,6 +181,8 @@ awful.screen.connect_for_each_screen(function(s)
     -- Create the wibox
     s.mywibox = awful.wibox({ height = beautiful.useless_gap * 2, width = awful.screen.focused().geometry.width - (4 * beautiful.useless_gap), ontop = true, screen = s })
 
+    
+
     -- Add widgets to the wibox
     s.mywibox:setup {
         layout = wibox.layout.align.horizontal,
@@ -191,11 +193,14 @@ awful.screen.connect_for_each_screen(function(s)
             s.mypromptbox,
         },
 
-        -- s.mytasklist, -- Middle widget
+        s.mytasklist, -- Middle widget
         wibox.widget.textclock(),
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
+            -- mykeyboardlayout,
             wibox.widget.systray(),
+            mytextclock,
+            -- s.mylayoutbox,
         },
     }
 end)
