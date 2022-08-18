fa6a180a9e569c327a28dd385a726f8fd2eb9b2a
David McCullah <davidwmccullah@gmail.com>
2022-08-18 04:14:03 -0500

    Attempting to add margin to wibar.

--- rc.lua
+++ rc.lua
@@ -193,7 +193,7 @@ awful.screen.connect_for_each_screen(function(s)
         margin = {
             top = beautiful.useless_gap * 2
         },
-        {
+        widget = {
             layout = wibox.layout.align.horizontal,
 
             -- Left widgets
