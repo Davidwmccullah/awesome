0b9c3de2c767e0846205f07922282875927e59e6
David McCullah <davidwmccullah@gmail.com>
2022-08-18 03:58:23 -0500

    Reverted to desired widgets.

--- rc.lua
+++ rc.lua
@@ -197,14 +197,14 @@ awful.screen.connect_for_each_screen(function(s)
             s.mypromptbox,
         },
 
-        s.mytasklist, -- Middle widget
+        -- s.mytasklist, -- Middle widget
         wibox.widget.textclock(),
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
+            -- mykeyboardlayout,
             wibox.widget.systray(),
-            mytextclock,
-            s.mylayoutbox,
+            -- mytextclock,
+            -- s.mylayoutbox,
         },
     }
 end)
