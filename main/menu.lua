be3c68b7e156b8e0c0002448685283aa9332e2ea
David McCullah <davidwmccullah@gmail.com>
2022-08-19 04:12:23 -0500

    Updated menu, forgot a comma.

--- main/menu.lua
+++ main/menu.lua
@@ -3,7 +3,7 @@ local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 local user_variables = require("main.user_variables")
 
-local menu = awful.menu({ items = { { "open terminal", user_variables.terminal }
+local menu = awful.menu({ items = { { "open terminal", user_variables.terminal },
                                     { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
                                     { "restart", awesome.restart },
                                     { "quit", function() awesome.quit() end },
