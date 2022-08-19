829cf5ec9a2949e490bbe0359e7387ffa4ba8c5d
David McCullah <davidwmccullah@gmail.com>
2022-08-19 03:16:16 -0500

    Updated user-variables.

--- rc.lua
+++ rc.lua
@@ -22,25 +22,7 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 -- Error handling
 require("main.error-handling")
 
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
-beautiful.init("~/.config/awesome/theme.lua")
-
--- This is used later as the default terminal and editor to run.
-terminal = "alacritty"
-editor = "vim" 
-editor_cmd = terminal .. " -e " .. editor
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
-
--- Table of layouts to cover with awful.layout.inc, order matters.
-awful.layout.layouts = { awful.layout.suit.tile }
--- }}}
+local user_variables = require("main.user-variables")
 
 -- {{{ Menu
 -- Create a launcher widget and a main menu
