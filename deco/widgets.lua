a5aea2e7c6e064c9c6049ca3f7e8cf04a9cfad3c
David McCullah <davidwmccullah@gmail.com>
2022-08-19 05:48:02 -0500

    Changing statusbar layout.

--- deco/widgets.lua
+++ deco/widgets.lua
@@ -2,21 +2,23 @@ local awful = require("awful")
 local gears = require("gears")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
+local naughty = require("naughty")
 
 awful.screen.connect_for_each_screen(function(s)
     s.clock = wibox.widget.textclock()
     s.tray = wibox.widget.systray()
-    s.prompt = awful.popup {
-        widget = {
-            widget  = wibox.container.margin,
-            margin = 2 * beautiful.useless_gap,
-            {
-                widget = wibox.widget.background,
-                {
-                    text   = 'foobar',
-                    widget = wibox.widget.textbox
-                }
-            }   
-        }
+    s.prompt = awful.widget.prompt.run {
+        prompt = "<b>Spotify Shell</b>: ",
+        bg_cursor = '#84bd00',
+        textbox = wibox.widget.textbox {
+            text = "test"
+        },
+        exe_callback = function(input_text)
+            if not input_text or #input_text == 0 then return end
+            awful.spawn("sp " .. input_text)
+        end,
+        done_callback = function(p)
+            p.visible = false
+        end
     }
 end)
