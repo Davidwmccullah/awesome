eb132474c0685fd402deb317070ad43a47e8184e
David McCullah <davidwmccullah@gmail.com>
2022-08-19 05:16:01 -0500

    Changing statusbar layout.

--- deco/statusbar.lua
+++ deco/statusbar.lua
@@ -4,16 +4,17 @@ local beautiful = require("beautiful")
 local shapes = require("deco.shapes")
 
 awful.screen.connect_for_each_screen(function(s)
-    s.wiboxpadding = awful.wibox {
-        height = 2 * beautiful.useless_gap,
-        opacity = 0,
-    }
+    -- s.wiboxpadding = awful.wibox {
+    --     height = 2 * beautiful.useless_gap,
+    --     opacity = 0,
+    -- }
 
     s.statusbar = awful.wibox {
-        width = awful.screen.focused().geometry.width - (2 * beautiful.useless_gap),
+        -- width = awful.screen.focused().geometry.width - (4 * beautiful.useless_gap),
         ontop = true,
         screen = s,
         shape = shapes.rrect,
+        bg = "#00000000",
         widget = wibox.widget {
             layout = wibox.layout.align.horizontal,
             expand = "none",
