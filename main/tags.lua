674e4381671005500793a079daed2d40fbb35e45
David McCullah <davidwmccullah@gmail.com>
2022-08-19 03:36:45 -0500

    Updated tags.

--- main/tags.lua
+++ main/tags.lua
@@ -1,6 +1,6 @@
 local awful = require("awful")
 
 -- Each screen has its own tag table.
-awful.connect_for_each_screen(function(s)
+awful.screen.connect_for_each_screen(function(s)
     awful.tag({ "1", "2", "3", "4", "5"}, s, awful.layout.layouts[1])
 end)
