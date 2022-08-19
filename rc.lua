2bc8e6145a908906ed08626df0faef428b9e2b73
David McCullah <davidwmccullah@gmail.com>
2022-08-19 04:32:55 -0500

    Cleaning up variables.

--- rc.lua
+++ rc.lua
@@ -3,18 +3,6 @@
 local awful = require("awful")
 require("awful.autofocus")
 
--- Widget and layout library
-local wibox = require("wibox")
-
--- Theme handling library
-local beautiful = require("beautiful")
-local shapes = require("deco.shapes")
-
--- Notification library
--- local naughty = require("naughty")
--- local menubar = require("menubar")
--- local hotkeys_popup = require("awful.hotkeys_popup")
-
 -- Error handling
 require("main.error-handling")
 
@@ -33,17 +21,15 @@ local globalkeys = require("binding.globalkeys")
 local globalbuttons = require("binding.globalbuttons")
 
 -- Rice
-local wallpaper = require("deco.wallpaper")
-local taglist = require("deco.taglist")
-local tasklist = require("deco.tasklist")
-local statusbar = require("deco.statusbar")
+require("deco.wallpaper")
+require("deco.taglist")
+require("deco.tasklist")
+require("deco.statusbar")
 
 awful.screen.connect_for_each_screen(function(s)
     s.clock = wibox.widget.textclock()
     s.tray = wibox.widget.systray()
     s.mypromptbox = awful.widget.prompt()
-
-
 end)
 -- }}}
 
