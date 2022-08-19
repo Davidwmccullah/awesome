5f9e3d4ce6921855e0c5405347125370a64d0f98
David McCullah <davidwmccullah@gmail.com>
2022-08-19 05:37:05 -0500

    Changing statusbar layout.

--- deco/widgets.lua
+++ deco/widgets.lua
@@ -1,42 +1,21 @@
 local awful = require("awful")
 local gears = require("gears")
 local wibox = require("wibox")
+local beautiful = require("beautiful")
 
 awful.screen.connect_for_each_screen(function(s)
     s.clock = wibox.widget.textclock()
     s.tray = wibox.widget.systray()
     s.prompt = awful.popup {
         widget = {
+            widget  = wibox.container.margin,
+            margin = 2 * beautiful.useless_gap,
             {
+                widget = wibox.widget.background,
                 {
                     text   = 'foobar',
                     widget = wibox.widget.textbox
-                },
-                {
-                    {
-                        text   = 'foobar',
-                        widget = wibox.widget.textbox
-                    },
-                    bg     = '#ff00ff',
-                    clip   = true,
-                    shape  = gears.shape.rounded_bar,
-                    widget = wibox.widget.background
-                },
-                {
-                    value         = 0.5,
-                    forced_height = 30,
-                    forced_width  = 100,
-                    widget        = wibox.widget.progressbar
-                },
-                layout = wibox.layout.fixed.vertical,
-            },
-            margins = 10,
-            widget  = wibox.container.margin
-        },
-        border_color = '#00ff00',
-        border_width = 5,
-        placement    = awful.placement.top_left,
-        shape        = gears.shape.rounded_rect,
-        visible      = true,
-    }
+                }
+            }   
+        }
 end)
