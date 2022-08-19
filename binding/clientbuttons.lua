af1aabbde86d76a3aad2eeee49f4d1c880ae9c79
David McCullah <davidwmccullah@gmail.com>
2022-08-19 03:45:51 -0500

    Updated rules, clientkeys, clientbuttons.

---
+++ binding/clientbuttons.lua
@@ -0,0 +1,19 @@
+local awful = require("awful")
+local gears = require("gears")
+local user_variables = require("main.user_variables")
+
+local clientbuttons = gears.table.join(
+    awful.button({ }, 1, function (c)
+        c:emit_signal("request::activate", "mouse_click", {raise = true})
+    end),
+    awful.button({ user_variables.modkey }, 1, function (c)
+        c:emit_signal("request::activate", "mouse_click", {raise = true})
+        awful.mouse.client.move(c)
+    end),
+    awful.button({ user_variables.modkey }, 3, function (c)
+        c:emit_signal("request::activate", "mouse_click", {raise = true})
+        awful.mouse.client.resize(c)
+    end)
+)
+
+return clientbuttons
